930708b65fdcefb4d13fdad20fcc093dee48d5dc
oliviasqw <91005199+oliviasqw@users.noreply.github.com>
2021-09-30 14:02:18 +0800

    Add files via upload
    for practical lesson 1

---
+++ assignment_1.R
@@ -0,0 +1,12 @@
+# for practice assignment 01
+
+print(1:10)
+
+i = 0 
+
+# print all integers between 1 and 10
+
+for(i in 0:9) {
+  i = i+1
+  print(i)
+}
---
+++ spyder_assignment_1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 00:04:53 2021
+
+@author: Olivia
+"""
+
+# for assignment 1
+# print all integers between 1 and 10 
+
+i = 0
+for i in range(10):
+    i = i+1 
+    print(i)
